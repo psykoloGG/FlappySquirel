27c07296152dece8d8843f992a4c2690140a57c1
Lukas Kundelis <lukas.kundelis@gmail.com>
2024-12-07 18:19:45 +0000

    added project files

---
+++ Source/FlappySquirrel/FlappySquirrel.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FlappySquirrel.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlappySquirrel, "FlappySquirrel" );
